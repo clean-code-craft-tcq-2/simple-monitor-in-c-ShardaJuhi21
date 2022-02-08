02dc4de606ca31130eba5d9e1d10b16b1311e448
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-02-08 16:17:15 +0530

    Created CheckTemp

--- checker.c
+++ checker.c
@@ -1,21 +1,60 @@
 #include <stdio.h>
 #include <assert.h>
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
+
+void printToConsole( char *StringeName){
+printf("%s out of range!\n", StringeName);
+}
+
+float checkTemp(float temp , void (*fpPrint)(char *)){
+ char StringeName[15] = "Temperature";
+  float chargeRate=0.8;
+ float temperature = temp;
+ if(temperature < 0 || temperature > 45) {
+    fpPrint(StringeName);
+    return chargeRate;
   }
   return 1;
 }
 
+// int checkSoc(float Soc,float temp, fpTemp(temp, fprint)){
+//   char StringeName[15] = "Soc";
+//  if(soc < 20 || soc > 80) {
+//     result = checkTemp(float , fpprint);
+//     fpprint(StringeName);
+//     return 0;
+//   } 
+//   return 1;
+// }
+
+// float checkChargeRate(float ChargeRate,float soc,void (*fpPrint)(char *)){
+//   char StringeName[15] = "ChargeRate";
+//   float Soc=soc;
+//   float chargeRate = ChargeRate;
+//   (chargeRate > 0.8) {
+//     fpPrint(StringeName);
+//     return 0;
+//   }
+//   return 1;
+// }
+
+
+int batteryIsOk(float temp,void (*fpPrint)(char *),float (*fpCheckTemp)(float , void (fpPrint)(char * )) {
+  char StringeName[15] = "ChargeRate";
+  float temperature = temp;
+  float ChargeRate = fpCheckTemp(temperature , fpPrint);
+    fpPrint(StringeName);
+    return 0;
+  }
+ 
+  return 1;
+  
+}
+
+
+
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+  assert(batteryIsOk(50,printToConsole,checkTemp));
+//   assert(batteryIsOk(25, 70, 0.7));
+//   assert(!batteryIsOk(50, 85, 0));
 }
